b5a2e64bd7686829df8f36672d6cd37cb5cbbd1e
Kaitlyn Crawford <kaitlyn@praekelt.com>
2015-09-11 17:11:42 +0200

    show 'is reported' in the comment list display

--- molo/commenting/admin.py
+++ molo/commenting/admin.py
@@ -7,7 +7,14 @@ from django_comments.admin import CommentsAdmin
 
 
 class MoloCommentAdmin(CommentsAdmin):
-    list_display = ('comment', 'content_object', 'user', 'is_removed', 'submit_date')
+    list_display = ('comment', 'content_object', 'user', 'is_removed', 'is_reported', 'submit_date')
+    
+    def is_reported(self, obj):
+        if (obj.flags.count()>0):
+            return True
+        return False
+    is_reported.admin_order_field = 'is_reported'
+    is_reported.boolean = True
     
 
 admin.site.register(MoloComment, MoloCommentAdmin)
