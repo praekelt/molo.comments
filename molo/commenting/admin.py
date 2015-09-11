ae89dace5d46725f714bea5f649cd13ad13006f2
Kaitlyn Crawford <kaitlyn@praekelt.com>
2015-09-11 17:10:35 +0200

    add details to the comment list display

--- molo/commenting/admin.py
+++ molo/commenting/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from molo.commenting.models import MoloComment
 from django_comments.models import CommentFlag
+from django_comments.admin import CommentsAdmin
 
 
-admin.site.register(MoloComment)
+
+class MoloCommentAdmin(CommentsAdmin):
+    list_display = ('comment', 'content_object', 'user', 'is_removed', 'submit_date')
+    
+
+admin.site.register(MoloComment, MoloCommentAdmin)
 admin.site.register(CommentFlag)
