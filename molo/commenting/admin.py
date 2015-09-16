4cd86a48be34b06fca0c5b7e68c9227395dc6658
Kaitlyn Crawford <kaitlyn@praekelt.com>
2015-09-16 18:30:37 +0200

    added a new ModelAdmin subclass that shows all comments connected to a given article

--- molo/commenting/admin.py
+++ molo/commenting/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
 from molo.commenting.models import MoloComment
+from molo.core.models import ArticlePage
 from django_comments.models import CommentFlag
 from django_comments.admin import CommentsAdmin
 from django.core.urlresolvers import reverse
+from django.core.exceptions import PermissionDenied
 from django.contrib.admin.views.main import ChangeList
+from django.shortcuts import get_object_or_404
+from django.contrib.admin.utils import unquote
 from django.contrib.contenttypes.models import ContentType
 
 
@@ -85,5 +89,79 @@ class MoloCommentAdmin(CommentsAdmin):
 
         return ModeratorChangeList
 
+
+class AdminModeratorMixin(admin.ModelAdmin):
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        """
+        Override change view to add extra context enabling moderate tool.
+        """
+        context = {
+            'has_moderate_tool': True
+        }
+        if extra_context:
+            context.update(extra_context)
+        return super(AdminModeratorMixin, self).change_view(
+            request=request,
+            object_id=object_id,
+            form_url=form_url,
+            extra_context=context
+        )
+
+    def get_urls(self):
+        """
+        Add aditional moderate url.
+        """
+        from django.conf.urls import patterns, url
+        urls = super(AdminModeratorMixin, self).get_urls()
+        info = self.model._meta.app_label, self.model._meta.model_name
+        return patterns(
+            '',
+            url(r'^(.+)/moderate/$',
+                self.admin_site.admin_view(self.moderate_view),
+                name='%s_%s_moderate' % info),
+        ) + urls
+
+    def moderate_view(self, request, object_id, extra_context=None):
+        """
+        Handles moderate object tool through a somewhat hacky changelist view
+        whose queryset is altered via MoloCommentAdmin.get_changelist to only
+        list comments for the object under review.
+        """
+        opts = self.model._meta
+        app_label = opts.app_label
+
+        view = MoloCommentAdmin(model=MoloComment, admin_site=self.admin_site)
+
+        view.list_filter = ()
+        view.list_display = (
+            'comment',
+            'content_object',
+            '_user',
+            'submit_date',
+        )
+
+        model = self.model
+        obj = get_object_or_404(model, pk=unquote(object_id))
+        request.obj = obj
+        view.change_list_template = self.change_list_template or [
+            'admin/%s/%s/moderate.html' % (app_label, opts.object_name.lower()),
+            'admin/%s/moderate.html' % app_label,
+            'admin/moderate.html'
+        ]
+        orig_has_change_permission = self.has_change_permission(request, obj)
+        if not orig_has_change_permission:
+            raise PermissionDenied
+        extra_context = {
+            'opts': opts,
+            'original': obj,
+            'orig_has_change_permission': orig_has_change_permission,
+        }
+        return view.changelist_view(request, extra_context)
+
+
+class ModeratedPageAdmin(AdminModeratorMixin, admin.ModelAdmin):
+    pass
+
 admin.site.register(MoloComment, MoloCommentAdmin)
 admin.site.register(CommentFlag)
+admin.site.register(ArticlePage, ModeratedPageAdmin)
