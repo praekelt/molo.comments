702e0eafe2910fbdcac166c75df3492da59ffcbc
Richard Landers <infolog>
2016-04-07 15:27:59 +0200

    TUNE-72: canned responses

--- molo/commenting/forms.py
+++ molo/commenting/forms.py
@@ -1,8 +1,30 @@
 from django import forms
-from django_comments.forms import CommentForm
+from django.db import OperationalError
 from django.utils.translation import ugettext_lazy as _
+from django_comments.forms import CommentForm
+from molo.commenting.models import MoloComment, CannedResponse
 
-from molo.commenting.models import MoloComment
+
+class ReplyCommentForm(CommentForm):
+    """
+    Overriding to remove dupe comment check
+    """
+    def get_comment_object(self):
+        """
+        Return a new (unsaved) comment object based on the information in this
+        form. Assumes that the form is already validated and will throw a
+        ValueError if not.
+
+        Does not set any of the fields that would come from a Request object
+        (i.e. ``user`` or ``ip_address``).
+        """
+        if not self.is_valid():
+            raise ValueError("get_comment_object may only be called on valid forms")
+
+        CommentModel = self.get_comment_model()
+        new = CommentModel(**self.get_comment_create_data())
+
+        return new
 
 
 class MoloCommentForm(CommentForm):
@@ -22,7 +44,17 @@ class MoloCommentForm(CommentForm):
         return data
 
 
-class MoloCommentReplyForm(CommentForm):
+def get_canned_choices():
+    canned_choices = [('', '---------')]
+    try:
+        canned_choices += ([(canned.response, canned.response_header) for canned in CannedResponse.objects.all()])
+    except OperationalError:
+        canned_choices = []
+
+    return canned_choices
+
+
+class MoloCommentReplyForm(ReplyCommentForm):
     parent = forms.ModelChoiceField(
         queryset=MoloComment.objects.all(), widget=forms.HiddenInput,
         required=False)
@@ -37,5 +69,6 @@ class MoloCommentReplyForm(CommentForm):
 
     def __init__(self, *args, **kwargs):
         parent = MoloComment.objects.get(pk=kwargs.pop('parent'))
-        return super(MoloCommentReplyForm, self).__init__(
-            parent.content_object, *args, **kwargs)
+        super(MoloCommentReplyForm, self).__init__(parent.content_object, *args, **kwargs)
+        self.fields['canned_select'] = forms.ChoiceField(choices=get_canned_choices(),
+                                                         label="Or add a Canned response", required=False)
