3ddf91eb3119165a1030cbac521da06554cb42c3
Milton Madanda <madandat@gmail.com>
2016-05-31 08:44:23 +0200

    fix tests for django 1.9

--- test_settings.py
+++ test_settings.py
@@ -1,8 +1,8 @@
 from .base import INSTALLED_APPS
 
-INSTALLED_APPS = INSTALLED_APPS + (
+INSTALLED_APPS = INSTALLED_APPS + [
     'django_comments',
-)
+]
 
 COMMENTS_APP = 'molo.commenting'
 SITE_ID = 1
