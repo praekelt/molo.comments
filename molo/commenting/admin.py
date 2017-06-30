c8d2956ea435d97c26f72286227a1a0e07c9f73a
Codie Roelf <codie@praekelt.com>
2017-06-30 14:14:51 +0200

    encode parent comments in unicode

--- molo/commenting/admin.py
+++ molo/commenting/admin.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+import sys
 from django_comments.models import CommentFlag
 from django_comments.admin import CommentsAdmin
 from django.contrib import admin
@@ -281,6 +283,8 @@ class MoloCommentsModelAdmin(WagtailModelAdmin, MoloCommentAdmin):
     moderator_reply.allow_tags = True
 
     def parent_comment(self, obj):
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
         if obj.parent:
             return format_html(
                 '<a href="{}">{}</a>',
